b69d538d18730a743d422f4eb81694fa4d8dd756
retrysamurai <fateen.mostafa@gmail.com>
2022-12-19 03:06:28 +0600

    started working on dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -8,7 +8,41 @@ export function Dashboard() {
     <div className="container">
       <div className="dashboard">
         <div className="actions">
-          <h1>Actions</h1>
+          <div className="action">
+            <p>Send Money</p>
+            <form>
+              <input type="text" placeholder="Amount" />
+              <input type="text" placeholder="Email" />
+              <button type="submit">Send Money</button>
+            </form>
+          </div>
+          <div className="action">
+            <p>Cash Out</p>
+            <form>
+              <input type="text" placeholder="Amount" />
+              <input type="text" placeholder="Agent Email" />
+              <button type="submit">Cash Out</button>
+            </form>
+          </div>
+          <div className="action">
+            <p>Pay Bill</p>
+            <form>
+              <input type="text" placeholder="Amount" />
+              <select>
+                <option value="BRAC Bank">BRAC Bank</option>
+                <option value="SIBL">SIBL</option>
+                <option value="Prime Bank">Prime Bank</option>
+                <option value="Arafah Bank">Arafah Bank</option>
+                <option value="DBBL">DBBL</option>
+              </select>
+              <select>
+                <option value="Water Bill">Water Bill</option>
+                <option value="Electricity Bill">Electricity Bill</option>
+                <option value="Gas Bill">Gas Bill</option>
+              </select>
+              <button type="submit">Cash Out</button>
+            </form>
+          </div>
         </div>
         <div className="history">
           <h1>History</h1>
